174b042e14cfd719d56b25ff3125b885d6287aca
vadivel-vital <vadivel@vtldesign.com>
2025-04-26 09:04:50 +0530

    make android apk

---
+++ android/app/.cxx/RelWithDebInfo/162t6013/prefab/arm64-v8a/prefab/lib/aarch64-linux-android/cmake/hermes-engine/hermes-engineConfig.cmake
@@ -0,0 +1,9 @@
+if(NOT TARGET hermes-engine::libhermes)
+add_library(hermes-engine::libhermes SHARED IMPORTED)
+set_target_properties(hermes-engine::libhermes PROPERTIES
+    IMPORTED_LOCATION "/Users/vadivel/.gradle/caches/8.13/transforms/e561a215717d620392217fceaffb1ff8/transformed/hermes-android-0.79.1-release/prefab/modules/libhermes/libs/android.arm64-v8a/libhermes.so"
+    INTERFACE_INCLUDE_DIRECTORIES "/Users/vadivel/.gradle/caches/8.13/transforms/e561a215717d620392217fceaffb1ff8/transformed/hermes-android-0.79.1-release/prefab/modules/libhermes/include"
+    INTERFACE_LINK_LIBRARIES ""
+)
+endif()
+
