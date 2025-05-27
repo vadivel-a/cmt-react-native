3e34d0e6265e49f10898fc37bda147ff4d19eec5
vadivel-vital <vadivel@vtldesign.com>
2025-05-27 22:28:01 +0530

    added deep link

--- App.js
+++ App.js
@@ -4,12 +4,27 @@ import { store, persistor } from './src/store/store';
 import { PersistGate } from 'redux-persist/integration/react';
 import { NavigationContainer } from '@react-navigation/native';
 import RootNavigator from './src/navigations/RootNavigator.js';
+import { ForgotPassword } from './src/screens';
+
+const linking = {
+  prefixes: [
+    'mandyportal://', // custom scheme
+    'https://portal.mandyintranet.in', // web
+  ],
+  config: {
+    screens: {
+      Home: '',
+      ForgotPassword: 'forgot-password',
+      // Add other routes
+    },
+  },
+};
 
 export default function App() {
   return (
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <NavigationContainer>
+        <NavigationContainer linking={linking}>
           <RootNavigator />
         </NavigationContainer>
       </PersistGate>
